44fc1af082d107d50c5a7af3754b75594c357acd
Gleb <propeller@e1.ru>
2022-05-09 23:08:04 +0500

    make it better

---
+++ alisa.py
@@ -0,0 +1,78 @@
+# coding=utf-8
+from flask import Flask, request
+import logging
+import json
+
+
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.INFO)
+sessionStorage = {}
+
+
+@app.route('/post', methods=['POST'])
+def main():
+    logging.info(f'Request: {request.json!r}')
+
+    # Начинаем формировать ответ
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+    handle_dialog(request.json, response)
+    logging.info(f'Response:  {response!r}')
+
+    # Преобразовываем в JSON и возвращаем
+    return json.dumps(response)
+
+
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Не хочу.",
+                "Не буду.",
+                "Отстань!",
+            ]
+        }
+        # Заполняем текст ответа
+        res['response']['text'] = 'Привет! Купи слона!'
+        # Получим подсказки
+        res['response']['buttons'] = get_suggests(user_id)
+        return
+    # Сюда дойдем только, если пользователь не новый, и разговор с Алисой уже был начат
+    text = req['request']['original_utterance'].lower()
+    if True in {t in text for t in {'ладно', 'куплю', 'покупаю', 'хорошо'}}:
+        res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
+        res['response']['end_session'] = True
+        return
+    res['response']['text'] = f"Все говорят '{req['request']['original_utterance']}', а ты купи слона!"
+    res['response']['buttons'] = get_suggests(user_id)
+
+
+# Функция возвращает две подсказки для ответа.
+def get_suggests(user_id):
+    session = sessionStorage[user_id]
+
+    suggests = [{'title': suggest, 'hide': True} for suggest in session['suggests'][:2]]
+    session['suggests'] = session['suggests'][1:]
+    sessionStorage[user_id] = session
+
+    # Если осталась только одна подсказка, предлагаем подсказку со ссылкой на Яндекс.Маркет.
+    if len(suggests) <= 1:
+        suggests.append({
+            "title": "Ладно",
+            "url": "https://market.yandex.ru/search?text=слон",
+            "hide": True
+        })
+
+    return suggests
+
+
+if __name__ == '__main__':
+    app.run()
